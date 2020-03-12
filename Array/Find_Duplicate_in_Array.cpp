fa0a69d7648c0fc317c694ffb530c6229260ae23
Aman Kumar <amanalphakumar@gmail.com>
2020-03-13 01:10:21 +0530

    Create Find_Duplicate_in_Array.cpp

---
+++ Array/Find_Duplicate_in_Array.cpp
@@ -0,0 +1,30 @@
+// Using Floyd's slow and fast pointers.
+
+int Solution::repeatedNumber(const vector<int> &A) 
+{
+    int slow = A[0] ;       // slow = head
+    int fast = A[ A[0] ] ;  // fast = slow -> next
+    int ret = -1 ;
+    
+    while( slow != fast )
+    {
+        slow = A[ slow ] ;      // slow = slow -> next 
+        fast = A[ A[fast] ] ;   // fast = fast -> next -> next 
+    }
+    
+    slow = 0 ;
+    
+    // Finding the duplicate number
+    while( slow != fast )
+    {
+        slow = A[ slow ] ;      // slow = slow -> next 
+        fast = A[ fast ] ;      // fast = fast -> next 
+    }
+
+    if( slow == 0 )
+        ret = -1 ;
+    else
+        ret = slow ;
+  
+    return ret ;
+}
