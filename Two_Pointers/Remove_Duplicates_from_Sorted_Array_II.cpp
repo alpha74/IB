20577caa6015618ab3beb0f40b2dcbd797bde1b3
Aman Kumar <amanalphakumar@gmail.com>
2020-03-14 11:21:42 +0530

    Create Remove_Duplicates_from_Sorted_Array_II.cpp

---
+++ Two_Pointers/Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -0,0 +1,16 @@
+// Two pointer logic.
+
+int Solution::removeDuplicates(vector<int> &A) 
+{   
+    int count = 0;
+    int size = A.size();
+    
+    for( int i = 0; i < size; i++ )
+    {
+        if( i < size-2 && A[i] == A[i+1] && A[i] == A[i+2] ) 
+            ;
+        else
+            A[ count++ ] = A[ i ] ;
+    }
+    return count;
+}
