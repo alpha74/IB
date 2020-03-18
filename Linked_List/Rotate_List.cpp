5b9cf398889a097930c732fbac861dd24f9afd95
Aman Kumar <amanalphakumar@gmail.com>
2020-03-18 21:19:02 +0530

    Create Rotate_List.cpp

---
+++ Linked_List/Rotate_List.cpp
@@ -0,0 +1,57 @@
+// Iterate till (size-B) rotations and connect apprpriate node.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::rotateRight(ListNode* A, int B) 
+{
+    long long int size = 0 ;
+    
+    // Count number of nodes
+    ListNode *temp, *last, *first ;
+    temp = A ;
+    
+    while( temp != NULL )
+    {
+        size++ ;
+        
+        // Get the last node
+        if( temp -> next == NULL )
+            last = temp ;
+        
+        temp = temp -> next ;
+    }
+
+    if( size > 1 )
+    {
+    	B = B % size ;
+    	int rot = size - B ;
+    	
+    	if( rot > 0 && B != 0 )
+    	{
+    		rot-- ;
+    		last -> next = A ;
+			
+			temp = A ;
+			
+			while( rot-- )
+			{
+				temp = temp -> next ;
+			}
+			
+			first = temp -> next ;
+			temp -> next = NULL ;
+		}
+		else
+			first = A ;     	
+	}
+	else
+		first = A ;
+    
+    return first ;
+}
