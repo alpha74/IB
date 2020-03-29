932e629e8d58836bbc9cecea8dbdbb01e27fb50e
Aman Kumar <amanalphakumar@gmail.com>
2020-03-29 20:42:03 +0530

    Create Grep_Log.bash

---
+++ Scripting/Basic_Shell_Commands/Grep_Log.bash
@@ -0,0 +1,6 @@
+- Given a log file named input. You have to write a bash script which prints all the lines which contains mysql commands.
+
+- Note:
+  - Assume that all the mysql commands start with mysql::
+
+`cat input | grep "mysql::" `
