e5f4c777a1d67a59974b5349e1133d960c2bf410
Aman Kumar <amanalphakumar@gmail.com>
2020-04-10 01:58:18 +0530

    Create Subset.cpp

---
+++ Programming/Backtracking/Subset.cpp
@@ -0,0 +1,24 @@
+// Backtracking trick.
+
+void backtrack(vector<int> &A, int i, vector<vector<int> > &all , vector<int> &subset )
+{
+    all.push_back( subset );
+    for( ; i < A.size() ; i++ )
+    {
+        subset.push_back( A[i] ) ;
+        backtrack( A, i+1, all, subset ) ;
+        subset.pop_back() ;
+    }
+}
+
+vector<vector<int>> Solution::subsets(vector<int> &A) 
+{
+    sort( A.begin(), A.end() ) ;
+    
+    vector<vector<int>> ret ;
+    vector<int> subset ;
+    
+    backtrack(A, 0, ret, subset );
+    
+    return ret ;
+}
