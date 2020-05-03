2f9808d0b9c9c021b29deeaa2dd83ea0b97c3ce6
Aman Kumar <amanalphakumar@gmail.com>
2020-05-03 23:03:56 +0530

    Create Magician_and_Chocolates.cpp

---
+++ Programming/Heaps_and_Maps/Magician_and_Chocolates.cpp
@@ -0,0 +1,32 @@
+// Use priority queue and get top element each time.
+// Remove it and push its half.
+
+#define MOD 1000000007
+   
+int Solution::nchoc(int A, vector<int> &B) 
+{
+
+    // Create set from B
+    priority_queue<int> ch ;
+    
+    for( int i = 0 ; i < B.size() ; i++ )
+        ch.push( B[i] ) ;
+    
+    long long int total = 0 ;
+    
+    while( A-- )
+    {
+        int val = ch.top() ;
+        
+        total = ( total + val % MOD ) % MOD ;
+        
+        val = ch.top() ;
+        
+        ch.pop() ;
+        ch.push( val/2) ;
+    }
+    
+    return total ;
+}
+
+
